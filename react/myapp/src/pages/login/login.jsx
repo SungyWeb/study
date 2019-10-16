f700f188f036d68af57520108a453974446b446e
孙国元 <503391278@qq.com>
2019-10-16 21:00:06 +0800

    update

---
+++ react/myapp/src/pages/login/login.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import './login.less';
+import logo from './imgs/logo.png';
+import { Form, Icon, Input, Button } from 'antd';
+
+class Login extends Component {
+
+  handleSubmit = (ev) => {
+    const form = this.props.form;
+    const values = form.getFieldsValue();
+    console.log(values);
+    ev.preventDefault();
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <div className='login'>
+        <header className='login-header'>
+          <img src={logo} alt="logo"/>
+          <h1>React项目：后台管理系统</h1>
+        </header>
+        <section className='login-section'>
+          <h2>用户登录</h2>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+              })(
+                <Input
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  placeholder="Username"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  placeholder="Password"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+        </section>
+      </div>
+    )
+  }
+}
+
+const LoginWrap = Form.create()(Login);
+export default LoginWrap;
