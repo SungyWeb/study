4673bb961070b6e3784ca8285b3ce7de64a90b07
孙国元 <503391278@qq.com>
2019-10-21 21:23:52 +0800

    update

---
+++ react/myapp/src/api/ajax.js
@@ -0,0 +1,32 @@
+/*
+发送ajax请求的函数模块
+封装axios
+函数的返回值是一个promise对象
+统一处理请求错误： 外面套一层promise
+*/
+
+import axios from 'axios';
+import { message } from 'antd';
+
+export default function ajax (url, data = {}, method = 'GET') {
+  return new Promise((resolve, reject) => {
+    /*
+      1. 执行ajax请求
+      2. 请求成功，调用resolve
+      3. 请求失败， 不能调用reject，而是提示异常信息 
+    */
+    let promise = null;
+    if (method === 'GET') {
+      promise = axios.get(url, {
+        params: data
+      });
+    } else {
+      promise = axios.post(url, data);
+    }
+    promise.then(response => resolve(response)).catch(err => {
+      // reject(err);   注意不能调用reject
+      message.error('请求出错了，地址' + url + ', 错误信息：' + err.message);
+    })
+  });
+  
+}
--- react/myapp/src/pages/login/login.jsx
+++ react/myapp/src/pages/login/login.jsx
@@ -2,16 +2,45 @@ import React, { Component } from 'react';
 import './login.less';
 import logo from './imgs/logo.png';
 import { Form, Icon, Input, Button } from 'antd';
+import { reqLogin } from '../../api';
 
 class Login extends Component {
 
   handleSubmit = (ev) => {
-    const form = this.props.form;
-    const values = form.getFieldsValue();
-    console.log(values);
     ev.preventDefault();
-  }
+    this.props.form.validateFields(async (err, values) => {
+      if (!err) {
+        // console.log('Received values of form: ', values);
+        const { username, password} = values;
+        const response = await regLogin(username, passwork);
+        const result = response.data;
+        if (result.status === 0) {
 
+        }else {
+          
+        }
+      }
+    });
+  }
+  validatePwd = (rule, value, callback) => {
+    if(!!value) {
+      const len = value.length;
+      const reg = /^(?=.*[0-9])(?=.*[a-zA-Z])(.{6,20})$/;
+      if(len < 6) {
+        callback('密码长度不能小于6位');
+      } else if (len > 20) {
+        callback('密码长度不能大于20位')
+      } else {
+        if(!reg.test(value)) {
+          callback('密码必须同时包含数字、字母')
+        }else {
+          callback();
+        }
+      } 
+    }else {
+      callback('密码不能位空');
+    }
+  }
   render() {
     const { getFieldDecorator } = this.props.form;
 
@@ -26,7 +55,12 @@ class Login extends Component {
           <Form onSubmit={this.handleSubmit} className="login-form">
             <Form.Item>
               {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input your username!' }],
+                rules: [
+                  { required: true, message: 'Please input your username!' },
+                  { min: 4, message: '最少4位!' },
+                  { max: 12, message: '最多12位!' },
+                  { pattern: /^[\w]{4,12}$/, message: '必须是数字字母下划线!' },
+                ],
               })(
                 <Input
                   prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -36,7 +70,7 @@ class Login extends Component {
             </Form.Item>
             <Form.Item>
               {getFieldDecorator('password', {
-                rules: [{ required: true, message: 'Please input your Password!' }],
+                rules: [{ validator: this.validatePwd }],
               })(
                 <Input
                   prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
