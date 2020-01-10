5ec9c932de06255f815c229711e29f6739f89f81
sungy <503391278@qq.com>
2020-01-10 16:28:18 +0800

    update

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -6,10 +6,43 @@ import {ClaComp} from './components/class-comp';
 import {GreetFn, GreetCla} from './components/default-props';
 import './App.css';
 
+type Item = {
+  id: number,
+  msg: string
+};
+type IProps = {
+  list: Item[]
+};
+
+const List: React.FC<IProps> = (props: IProps) => {
+
+  const clicked = (e: React.MouseEvent<HTMLLIElement, MouseEvent>, item: Item) => {
+    e.persist();
+    let li = e.target as HTMLLIElement;
+    console.log(li.innerText);
+    alert(item.id)
+  }
+  return (
+    <ul>
+      {
+        props.list.map(item => (
+          <li key={item.id} onClick={(e: React.MouseEvent<HTMLLIElement, MouseEvent>) => clicked(e, item)}>{item.msg}</li>
+        ))
+      }
+    </ul>
+  )
+}
+
 const App: React.FC = () => {
   const span = <span>title children</span>
+  const list = [
+    {id: 1, msg: 'a'},
+    {id: 2, msg: 'b'},
+    {id: 3, msg: 'c'},
+  ]
   return (
     <div className="App">
+      <List list={list}/>
       <div className="box">
         <h1>Function component</h1>
         <Comp1 msg={'哈哈哈'} />
--- react/myapp/README.md
+++
@@ -1,8 +0,0 @@
-# react
-
-## 引入andt 和 less
-> https://ant.design/docs/react/use-with-create-react-app-cn
-
-## 自定义配置webpack
-
-`yarn eject`
